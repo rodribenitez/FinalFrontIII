c6a917f490265a00f1e2fe7ddb0c77d09396d634
Rodrigo <brause06@gmail.com>
2022-12-03 20:27:16 -0300

    mejora del navbar3 - Rodrigo Brause

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar />
+        <Footer />
+      </BrowserRouter>
     </div>
-
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,53 +1,50 @@
-import React from 'react'
-import { BrowserRouter, Route,Routes} from "react-router-dom" 
-import Home from "../Routes/Home"
-import ContactoPage from "../Routes/Contact"
-import FavsPage from "../Routes/Favs"
-import DetailPage from "../Routes/Detail"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Routes/Home";
+import ContactoPage from "../Routes/Contact";
+import FavsPage from "../Routes/Favs";
+import DetailPage from "../Routes/Detail";
 import { Link } from "react-router-dom";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
 
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            DH ODONTO
-          </Typography>
-          <button>Change theme</button>
-        </Toolbar>
-      </AppBar>
-      <BrowserRouter>
-        <Link to="/Home">Ir a Home</Link>
-        <Link to="/Contacto">Ir a Contacto</Link>
-        <Link to="/Favs">Ir a Favs</Link>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            >
+              DH ODONTO
+            </Typography>
+            <Link to="/Home">Ir a Home</Link>
+            <Link to="/Contacto">Ir a Contacto</Link>
+            <Link to="/Favs">Ir a Favs</Link>
+            <button>Change theme</button>
+          </Toolbar>
+        </AppBar>
         <Routes>
           <Route path="/Home" element={<Home />} />
-            <Route path="/Contacto" element={<ContactoPage />} />
-            <Route path="/Favs" element={<FavsPage />} />
-            <Route path="/dentista/:id" element={<DetailPage />} />
-            <Route path="/" element={<Home />} />
+          <Route path="/Contacto" element={<ContactoPage />} />
+          <Route path="/Favs" element={<FavsPage />} />
+          <Route path="/dentista/:id" element={<DetailPage />} />
+          <Route path="/" element={<Home />} />
         </Routes>
-    </BrowserRouter>
-    </Box>
+      </Box>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
