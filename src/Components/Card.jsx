3bd1d4ae3e3d567e2baceb77eff1acf39ec0aca2
Rodrigo <brause06@gmail.com>
2022-12-03 19:45:20 -0300

    Arreglando las cards de MUI y renderizado - Rodrigo Brause

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
+import CardMUI from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import {  CardActionArea, CardActions } from '@mui/material';
 
 const Card = ({ name, username, id }) => {
@@ -18,13 +18,13 @@ const Card = ({ name, username, id }) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-      {/* <Card sx={{ maxWidth: 345 }}>
+      {<CardMUI sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image = {process.env.PUBLIC_URL + "images/doctor.jpg" }
+          alt="img"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
@@ -38,14 +38,7 @@ const Card = ({ name, username, id }) => {
       <CardActions>
       <button onClick={addFav} className="favButton">Add fav</button>
       </CardActions>
-    </Card> */}
-
-    <div>
-      
-      <p> {username} </p>
-      <p> {name} </p>
-    </div>
-        
+    </CardMUI>}        
     </div>
   );
 };
