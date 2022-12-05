083352f210d00813d35030394c2204fd19b35d76
Rodrigo <brause06@gmail.com>
2022-12-05 17:12:58 -0300

    Form, validaciones - Rodrigo Brause

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,67 +1,54 @@
-import * as React from 'react';
-import CardMUI from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {  CardActionArea, CardActions } from '@mui/material';
+import * as React from "react";
+import CardMUI from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea, CardActions } from "@mui/material";
 import { Link } from "react-router-dom";
-import GradeIcon from '@mui/icons-material/Grade';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import {
-  setFavInStorage,
-
-} from "./utils/localStorage";
+import GradeIcon from "@mui/icons-material/Grade";
+import { useState } from "react";
+import { useEffect } from "react";
+import { setFavInStorage } from "./utils/localStorage";
 
 const Card = ({ name, username, id }) => {
-
-  // const [name , setName] = useState('');
-  // const [username, setUsername] = useState('')
-  // const [id, setId] = useState('')
-
-  const odontologo = [];
-  const[items, setItems] = useState([])
- 
-
-  const addFav = ()=>{
-    setFavInStorage({name, username,id})
+  const addFav = () => {
+    setFavInStorage({ name, username, id });
     // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  };
 
-
-  
   return (
-        <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
+    <div className="card">
+      {/* En cada card deberan mostrar en name - username y el id */}
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-      {<CardMUI sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image = {process.env.PUBLIC_URL + "images/doctor.jpg" }
-          alt="img"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-          <Link to={`/dentista/${id}`}>{name}</Link>
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {username}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      <button onClick={addFav} className="favButton"><GradeIcon/></button>
-      
-      </CardActions>
-    </CardMUI>}        
+      {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+      {
+        <CardMUI sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={process.env.PUBLIC_URL + "images/doctor.jpg"}
+              alt="img"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <Link to={`/dentista/${id}`}>{name}</Link>
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {username}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <button onClick={addFav} className="favButton">
+              <GradeIcon />
+            </button>
+          </CardActions>
+        </CardMUI>
+      }
     </div>
-
-    
   );
 };
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,54 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const validarNombre = (value) => {
+    return value ? value.length > 5 : false;
+  };
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onSumbmitForm = (e) => {
+    e.preventDefault();
+    if (validarNombre(name) && /\S+@\S+\.\S+/.test(email)) {
+      document.querySelector("#mensaje").innerHTML = "Gracias " +
+      name +
+      ", te contactaremos cuando antes vía mail"
+    } else {
+      document.querySelector("#mensaje_error").innerHTML = "Por favor verifique su información nuevamente"
+    }
+  };
+
   return (
-    <div>
-      <form>
+    <>
+      <form onSubmit={onSumbmitForm}>
+        <input
+          id="input1"
+          placeholder="Full name"
+          value={name}
+          onChange={onChangeName}
+        />
+        <input
+          id="input2"
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
+        <button type="submit">Send</button>
       </form>
-    </div>
+      <p id="mensaje_error"></p>
+      <h3 id = "mensaje"> </h3>
+    </>
   );
 };
 
